45fe6d9e1671ec87593c8e5a1f635cae0f400ee0
Daniel Kirby <dkirby24.10@googlemail.com>
2024-04-13 14:51:01 +0100

    Update: add primevue component library

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,18 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  css: ["~/assets/css/main.css"],
+  css: [
+    "~/assets/css/main.css",
+    "primeicons/primeicons.css",
+    "primevue/resources/themes/aura-light-green/theme.css",
+  ],
+  modules: ["nuxt-primevue"],
+  primevue: {
+    components: {
+      include: "*",
+      prefix: "P",
+    },
+  },
   postcss: {
     plugins: {
       tailwindcss: {},
