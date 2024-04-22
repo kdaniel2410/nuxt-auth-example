fda2b517a30b58460126c0e005cf598ebb94503a
Daniel Kirby <dkirby24.10@googlemail.com>
2024-04-22 19:39:24 +0100

    Refactor: remove unused logout function

--- pages/index.vue
+++ pages/index.vue
@@ -5,12 +5,5 @@
 </template>
 
 <script lang="ts" setup>
-const logout = async () => {
-  await $fetch("/api/auth/logout", {
-    method: "POST",
-  }).catch((error) => {
-    console.log(error)
-  })
-  navigateTo("/")
-}
+
 </script>
